c9a947e477454322e2cb7f38c3c43030cc90a971
Jiajun Ou <garzonou@gmail.com>
2015-11-27 12:46:31 +0800

    increase the number of redis connection

--- src/eleme/main.go
+++ src/eleme/main.go
@@ -16,8 +16,8 @@ var redisPool *redis.Pool
 
 func newRedisPool(server string) *redis.Pool {
       return &redis.Pool{
-          MaxIdle: 80,
-          MaxActive: 12000, // max number of connections
+          MaxIdle: 1000,
+          MaxActive: 30000, // max number of connections
           IdleTimeout: 240 * time.Second,
           Dial: func () (redis.Conn, error) {
               c, err := redis.Dial("tcp", server)
